fe730f1eb39aab16995f5c4562661fc999bc866e
mlandHanze <120571801+EwwItsMike@users.noreply.github.com>
2023-12-16 10:24:27 +0100

    Old changes

---
+++ src/main/java/com/customdiscordpresence/CustomDiscordPresencePlugin.java
@@ -0,0 +1,35 @@
+package com.customdiscordpresence;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+		name = "Custom Discord Presence",
+		conflicts = "Discord"
+)
+public class CustomDiscordPresencePlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private CustomDiscordPresenceConfig config;
+
+	@Provides
+	CustomDiscordPresenceConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(CustomDiscordPresenceConfig.class);
+	}
+
+
+}
